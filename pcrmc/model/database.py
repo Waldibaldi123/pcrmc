ce6f1d74a2fbc087275104603acfcb517d4aaf44
Waldibaldi123 <dwalder@ucsd.edu>
2022-07-31 08:22:59 -0700

    Generalize and reafactor database functions

--- pcrmc/model/database.py
+++ pcrmc/model/database.py
@@ -7,7 +7,7 @@
 # _read(table: str) -> DBResponse
 # _write(table: str, data: Any) -> DBResponse
 # read(table: str, optional filters) -> DBResponse
-# create(table: str, data: Any) -> DBResponse
+# insert(table: str, data: Any) -> DBResponse
 # update(table: str, filters) -> DBResponse
 # delete(table: str, filters) -> DBResponse
 
@@ -20,17 +20,22 @@
 import configparser
 import json
 from pathlib import Path
-from typing import Any, Dict, List, NamedTuple
+from typing import Any, NamedTuple
 from pcrmc import DB_READ_ERROR, DB_WRITE_ERROR, JSON_ERROR,\
     SUCCESS, FILE_ERROR
+from pcrmc import config
 
 DEFAULT_DB_DIR_PATH = Path.home().joinpath(
         "." + Path.home().stem + "_pcrmc_db"
 )
-DB_FILE_NAMES = [
-    "contacts.json",
-    "meetings.json"
-]
+DB_FILE_NAMES = {
+    "contact": "contacts.json",
+    "meeting": "meetings.json"
+}
+DB_NEXT_ID_NAMES = {
+    "contact": "NextCID",
+    "meeting": "NextMID"
+}
 
 
 def get_database_path(config_file: Path) -> Path:
@@ -46,7 +51,7 @@ def init_database(db_path: Path) -> int:
         db_path.mkdir(exist_ok=True)
     except OSError:
         return FILE_ERROR
-    for db_file_name in DB_FILE_NAMES:
+    for _, db_file_name in DB_FILE_NAMES.items():
         try:
             db_file_path = db_path / db_file_name
             db_file_path.touch(exist_ok=True)
@@ -69,144 +74,134 @@ class DatabaseHandler:
     def __init__(self, db_path: Path) -> None:
         self._db_path = db_path
 
-    def read_contacts(
+    def _get_id_for_new_entry(
         self,
-        identifier_name: str = None,
-        identifier_value: Any = None
-    ) -> DBResponse:
+        table: str
+    ) -> int:
+        config_parser = configparser.ConfigParser()
+        config_parser.read(config.CONFIG_FILE_PATH)
+
+        next_id_name = DB_NEXT_ID_NAMES["table"]
+        id = int(config_parser["General"][next_id_name])
+        config_parser["General"][next_id_name] = str(id + 1)
         try:
-            data = json.loads(self._db_path.read_text())
-            contacts = data["Contacts"]
+            with config.CONFIG_FILE_PATH.open("w") as file:
+                config_parser.write(file)
+        except OSError:
+            return FILE_ERROR
+        return id
+
+    def _read(self, table: str) -> DBResponse:
+        try:
+            table_path = self._db_path / DB_FILE_NAMES[table]
+            read_data = json.loads(table_path.read_text())
         except json.JSONDecodeError:
             return DBResponse([], JSON_ERROR)
         except OSError:
             return DBResponse([], DB_READ_ERROR)
+        return DBResponse(read_data, SUCCESS)
 
-        if identifier_name is None or identifier_value is None:
-            return DBResponse(contacts, SUCCESS)
-
-        filtered_contacts = [c for c in contacts if
-                             identifier_name in c and
-                             c[identifier_name] == identifier_value]
-        return DBResponse(filtered_contacts, SUCCESS)
-
-    def create_contact(
-        self,
-        contact: dict
-    ) -> DBResponse:
-        contacts, error = self.read_contacts()
-        if error:
-            return DBResponse(contacts, error)
-        raise NotImplementedError
-
-    def write_contacts(self, contact_list: List[Dict[str, Any]]) -> DBResponse:
-        # TODO: refactor once other functions are generalized
+    def _write(self, table: str, data: Any) -> DBResponse:
         try:
-            data = json.loads(self._db_path.read_text())
-            data["Contacts"] = contact_list
-            new_data_str = json.dumps(data, indent=4)
-            self._db_path.write_text(new_data_str)
-            return DBResponse(new_data_str, SUCCESS)
+            table_path = self._db_path / DB_FILE_NAMES[table]
+            table_path.write_text(json.dumps(data, indent=4))
+        except json.JSONDecodeError:
+            return DBResponse([], JSON_ERROR)
         except OSError:
-            return DBResponse(contact_list, DB_WRITE_ERROR)
+            return DBResponse(data, DB_WRITE_ERROR)
+        return DBResponse(data, SUCCESS)
 
-    def delete_contacts(
+    def read(
         self,
-        identifier_name: str = None,
+        table: str,
+        identifier_field: str = None,
         identifier_value: Any = None
     ) -> DBResponse:
         """
-        Deletes all contacts with field_name and field_vale
-        Returns deteleted contacts
+        Read table entries where identifier_field == identifier_value
+        Read all table entries if no identifier given
         """
-        contacts, error = self.read_contacts()
+        read_data, error = self._read(table)
         if error:
-            return DBResponse(contacts, error)
+            return DBResponse(read_data, error)
 
-        if identifier_name is None or identifier_value is None:
-            kept_contacts, error = self.write_contacts([])
-            return DBResponse(contacts, error)
+        if identifier_field is None or identifier_value is None:
+            return DBResponse(read_data, SUCCESS)
 
-        # TODO: there has to be a better solution than these loops
-        keep_contacts = [c for c in contacts if
-                         (identifier_name not in c) or
-                         (identifier_name in c and
-                          not c[identifier_name] == identifier_value)]
-        delete_contacts = [c for c in contacts if
-                           identifier_name in c and
-                           c[identifier_name] == identifier_value]
-        kept_contacts, error = self.write_contacts(keep_contacts)
-        return DBResponse(delete_contacts, SUCCESS)
+        filtered_contacts = [c for c in read_data if
+                             identifier_field in c and
+                             c[identifier_field] == identifier_value]
+        return DBResponse(filtered_contacts, SUCCESS)
+
+    def insert(
+        self,
+        table: str,
+        data: dict
+    ) -> DBResponse:
+        """Insert data into table"""
+        read_data, error = self._read(table)
+        if error:
+            return DBResponse(data, error)
+
+        data["ID"] = self._get_id_for_new_entry(table)
+        read_data.append(data)
+        write_data, error = self._write(table, read_data)
+        return DBResponse(write_data, error)
 
     def update_contact(
         self,
-        identifier_name: str,
+        table: str,
+        identifier_field: str,
         identifier_value: Any,
-        field_name: str,
-        field_value: Any
+        update_field: str,
+        update_value: Any
     ) -> DBResponse:
         """
-        Updates all contacts matching identifier_name with identifier_value
-        with field_value at field_name, if field_name exists
+        Update table entries where identifier_field == identifier_value
+        with update_field = update_value if update_field exists
+        Returns updated entries
         """
-        contacts, error = self.read_contacts()
+        read_data, error = self._read(table)
         if error:
-            return DBResponse(contacts, error)
+            return DBResponse(read_data, error)
 
-        updated_contacts = []
-        for c in contacts:
-            if (identifier_name in c and
-                    c[identifier_name] == identifier_value):
-                if field_name in c:
-                    c[field_name] = field_value
-                    updated_contacts.append(c)
+        updated_entries = []
+        for entry in read_data:
+            if (identifier_field in entry and
+                    entry[identifier_field] == identifier_value):
+                if update_field in entry:
+                    entry[update_field] = update_value
+                    updated_entries.append(entry)
 
-        # TODO: should not write all at once, but one after the other
-        # TODO: this way write_contacts can return what was actually written
-        # TODO: but really not sure, because right now is more efficient
-        _, error = self.write_contacts(contacts)
-        return DBResponse(updated_contacts, error)
+        _, error = self._write(table, read_data)
+        return DBResponse(updated_entries, error)
 
-    def read_meetings(self) -> DBResponse:
-        try:
-            data = json.loads(self._db_path.read_text())
-            meetings_json = data["Meetings"]
-            return DBResponse(meetings_json, SUCCESS)
-        except json.JSONDecodeError:
-            return DBResponse([], JSON_ERROR)
-        except OSError:
-            return DBResponse([], DB_READ_ERROR)
+    def delete_contacts(
+        self,
+        table: str,
+        identifier_field: str = None,
+        identifier_value: Any = None
+    ) -> DBResponse:
+        """
+        Delete entries in table where identifier_name and identifier_value
+        Delete all entries in table if no identifier is given
+        Returns deteleted entries
+        """
+        read_data, error = self._read(table)
+        if error:
+            return DBResponse(read_data, error)
 
-    def write_meetings(self, meeting_list: List[Dict[str, Any]]) -> DBResponse:
-        try:
-            data = json.loads(self._db_path.read_text())
-            data["Meetings"] = meeting_list
-            new_data_str = json.dumps(data, indent=4)
-            self._db_path.write_text(new_data_str)
-            return DBResponse(new_data_str, SUCCESS)
-        except OSError:
-            return DBResponse(meeting_list, DB_WRITE_ERROR)
+        if identifier_field is None or identifier_value is None:
+            _, error = self._write(table, [])
+            return DBResponse(read_data, error)
 
-    def _get_new_contact_id(self, config_file: Path) -> int:
-        config_parser = configparser.ConfigParser()
-        config_parser.read(config_file)
-        id = int(config_parser["General"]["NextCID"])
-        config_parser["General"]["NextCID"] = str(id + 1)
-        try:
-            with config_file.open("w") as file:
-                config_parser.write(file)
-        except OSError:
-            return FILE_ERROR
-        return id
-
-    def get_new_meeting_id(self, config_file: Path) -> int:
-        config_parser = configparser.ConfigParser()
-        config_parser.read(config_file)
-        id = int(config_parser["General"]["NextMID"])
-        config_parser["General"]["NextMID"] = str(id + 1)
-        try:
-            with config_file.open("w") as file:
-                config_parser.write(file)
-        except OSError:
-            return FILE_ERROR
-        return id
+        # TODO: there has to be more pythonic solution than these loops
+        keep_entries = [entry for entry in read_data if
+                        (identifier_field not in entry) or
+                        (identifier_field in entry and
+                         not entry[identifier_field] == identifier_value)]
+        delete_entries = [entry for entry in read_data if
+                          identifier_field in entry and
+                          entry[identifier_field] == identifier_value]
+        _, error = self._write(table, keep_entries)
+        return DBResponse(delete_entries, SUCCESS)
